b96a1ee2700c8fa3acc3fba3e086c05e6567edbc
Jesus Palacios <>
2018-03-17 23:48:53 +0100

    He añadido la clase de de la excepción SudokuException y los métodos Inicializar y comprobar fila y columna

--- src/sudoku_jesuspalacios_ed/Sudoku.java
+++ src/sudoku_jesuspalacios_ed/Sudoku.java
@@ -7,18 +7,55 @@ package sudoku_jesuspalacios_ed;
 
 /**
  *
- * @author user
+ * @author Jesus Palacios Malagón
  */
 public class Sudoku {
     
     // Variables privadas de la clase
      public Sudoku()
      {
-
+            
      }
      public void inicializar()
      {
-     }
+         int[][] matriz = new int[9][9];
+         int matrizSudoku = 1;
+         
+   
+        switch (matrizSudoku){
+   
+            case 2:
+
+            matriz[0][0] = 7;matriz[0][4] = 5;matriz[0][6] = 4;matriz[1][0] = 4;
+            matriz[1][3] = 1;matriz[1][4] = 9;matriz[1][6] = 6;matriz[1][7] = 2;
+            matriz[1][8] = 7;matriz[2][2] = 6;matriz[2][8] = 9;matriz[3][0] = 9;
+            matriz[3][2] = 3;matriz[3][6] = 8;matriz[4][3] = 4;matriz[4][5] = 3;
+            matriz[5][2] = 8;matriz[5][6] = 5;matriz[5][8] = 2;matriz[6][0] = 5;
+            matriz[6][6] = 2;matriz[7][0] = 2;matriz[7][1] = 9;matriz[7][2] = 1;
+            matriz[7][4] = 4;matriz[7][5] = 7;matriz[7][8] = 8;matriz[8][2] = 7;
+            matriz[8][4] = 1;matriz[8][8] = 5;
+
+            break;
+
+
+            case 1:
+            default:
+
+            matriz[0][2] = 9;matriz[0][5] = 8;matriz[0][6] = 5; matriz[0][7] = 4;
+            matriz[1][8] = 7;matriz[2][1] = 5;matriz[2][2] = 4; matriz[2][4] = 9;
+            matriz[2][6] = 1;matriz[3][5] = 6;matriz[3][6] = 3; matriz[3][7] = 2;
+            matriz[4][1] = 8;matriz[4][2] = 2;matriz[4][6] = 4; matriz[4][7] = 1;
+            matriz[5][1] = 3;matriz[5][2] = 5;matriz[5][3] = 2; matriz[6][2] = 7;
+            matriz[6][4] = 3;matriz[6][6] = 2;matriz[6][7] = 5; matriz[7][0] = 9;
+            matriz[8][1] = 4;matriz[8][2] = 3;matriz[8][3] = 8; matriz[8][7] = 9;
+
+            break;
+
+            }
+
+            
+           }
+     
      @Override
      public String toString()
      {
@@ -26,7 +63,7 @@ public class Sudoku {
      return resultadoFinal;
      }
      public void modificarElemento(int fila, int columna, int elemento) throws
-    SudokuException //Da error porque todavia no esta creada la excepcion
+    SudokuException 
      {
          
      }
@@ -35,14 +72,27 @@ public class Sudoku {
      {
 
      }
-     private boolean comprobarFila(int fila, int elemento)
+     private boolean comprobarFila(int fila, int elemento, int[][] matriz)
      {
-     boolean resultado = true;
+     boolean resultado = false;
+         int numero = 0;
+     for ( int i = 0; i < matriz.length; i ++ )
+        if ( matriz[(fila-1)][i] == numero ){
+         resultado = true;
+        }
      return resultado;
      }
-     private boolean comprobarColumna(int columna, int elemento)
+     
+     private boolean comprobarColumna(int columna, int elemento, int[][] matriz)
      {
-     boolean resultado = true;
+     
+      boolean resultado = false;
+         int numeroColumna = 0;
+   
+      for ( int i = 0; i < matriz.length; i ++ )
+        if ( matriz[i][(columna-1)] == numeroColumna ){
+        resultado = true;
+    }
      return resultado;
      }
      private boolean comprobarCuadrante(int fila, int columna, int elemento)
---
+++ src/sudoku_jesuspalacios_ed/SudokuException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku_jesuspalacios_ed;
+
+/**
+ *
+ * @author user
+ */
+public class SudokuException extends Exception{
+    
+   public SudokuException(String mensaje)
+     {
+
+        super(mensaje);
+    }
+ 
+}
